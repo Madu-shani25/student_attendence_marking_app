200cf98c85ec3439224f68bd3a3cdd18c5de9db5
Madu-shani25 <maduwanthi2563@gmail.com>
2020-10-16 15:05:53 -0700

    added user authentication

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,43 @@
+import 'package:student_attendence_marking_app/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        title: Text('Easy QR'),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0.0,
+
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('LogOut'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          ),
+        ],
+
+      ),
+    body: Center(
+    child: FlatButton(
+    color: Colors.blue,
+    textColor: Colors.white,
+    padding: EdgeInsets.all(8.0),
+    splashColor: Colors.blueAccent,
+    onPressed: () {},
+    child: Text(
+    "Scan Now",
+    style: TextStyle(fontSize: 20.0),
+    ),
+    ),
+    ),
+    );
+  }
+}
